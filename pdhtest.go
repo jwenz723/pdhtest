a944b5b89018e534d14e164957b690d352d4f89d
jeff.wenzbauer <jwenz723@gmail.com>
2018-04-25 15:35:13 -0600

    made main print out a count of what error codes occur

--- pdhtest.go
+++ pdhtest.go
@@ -7,30 +7,28 @@ import (
 	"bufio"
 	"github.com/lxn/win"
 	"unsafe"
-	"time"
 )
 
 func main() {
 	const COUNTERS_FILE= "counters.txt"
 
-	statusChan := make(chan bool)
-	//done := make(chan struct{})
+	statusChan := make(chan uint32)
 	countersChannel := make(chan string)
 	go readCounterConfigFile(COUNTERS_FILE, countersChannel)
 	go processCounters(countersChannel, statusChan)
 
-	successCount := 0
-	failCount := 0
-	for success := range statusChan {
-		if success {
-			successCount++
-		} else {
-			failCount++
+	statusCounts := map[uint32]int{}
+	for status := range statusChan {
+		if _, ok := statusCounts[status]; !ok {
+			statusCounts[status] = 0
 		}
+		statusCounts[status]++
 	}
-	
-	//<- done
-	fmt.Printf("processed all counters. Success: %d. Failures: %d\n", successCount, failCount)
+
+	fmt.Println("Counts of error codes:")
+	for k, v := range statusCounts {
+		fmt.Printf("\t- %x: %d\n", k, v)
+	}
 }
 
 func readCounterConfigFile(file string, countersChannel chan string) {
@@ -51,72 +49,63 @@ func readCounterConfigFile(file string, countersChannel chan string) {
 	close(countersChannel)
 }
 
-func processCounters(countersChannel chan string, status chan bool) {
+func processCounters(countersChannel chan string, status chan uint32) {
 	var queryHandle win.PDH_HQUERY
-	ret := win.PdhOpenQuery(0, 0, &queryHandle)
-	if ret != win.ERROR_SUCCESS {
-		fmt.Println("PdhOpenQuery failed")
-	}
-
 	counterHandles := map[string]*win.PDH_HCOUNTER{}
 
-	for counter := range countersChannel {
-		var c win.PDH_HCOUNTER
-		ret = win.PdhValidatePath(counter)
-		if ret == win.PDH_CSTATUS_BAD_COUNTERNAME {
-			fmt.Println("PdhValidatePath failed", counter)
-			status <- false
-			continue
-		}
-
-		ret = win.PdhAddEnglishCounter(queryHandle, counter, 0, &c)
-		if ret != win.ERROR_SUCCESS &&
-			ret != win.PDH_CSTATUS_NO_OBJECT {
-			fmt.Println("PdhAddEnglishCounter failed", counter)
-			status <- false
-			continue
-		}
-
-		status <- true
-		counterHandles[counter] = &c
-	}
-
-	ret = win.PdhCollectQueryData(queryHandle)
+	ret := win.PdhOpenQuery(0, 0, &queryHandle)
 	if ret != win.ERROR_SUCCESS {
-		fmt.Println("PdhCollectQueryData failed")
+		status <- ret
 	} else {
-		// start data collection
-		for {
-			valCount := 0
+		for counter := range countersChannel {
+			var c win.PDH_HCOUNTER
+			ret = win.PdhValidatePath(counter)
+			if ret == win.PDH_CSTATUS_BAD_COUNTERNAME {
+				status <- ret
+				continue
+			}
+
+			ret = win.PdhAddEnglishCounter(queryHandle, counter, 0, &c)
+			if ret != win.ERROR_SUCCESS {
+				status <- ret
+				continue
+			}
+
+			status <- ret
+			counterHandles[counter] = &c
+		}
+
+		ret = win.PdhCollectQueryData(queryHandle)
+		if ret != win.ERROR_SUCCESS {
+			status <- ret
+		} else {
+			counterCount := 0
+			instanceCount := 0
 			ret := win.PdhCollectQueryData(queryHandle)
 			if ret == win.ERROR_SUCCESS {
 				for _, v := range counterHandles {
 					var bufSize uint32
 					var bufCount uint32
-					var size = uint32(unsafe.Sizeof(win.PDH_FMT_COUNTERVALUE_ITEM_DOUBLE{}))
+					var size= uint32(unsafe.Sizeof(win.PDH_FMT_COUNTERVALUE_ITEM_DOUBLE{}))
 					var emptyBuf [1]win.PDH_FMT_COUNTERVALUE_ITEM_DOUBLE // need at least 1 addressable null ptr.
 
 					ret = win.PdhGetFormattedCounterArrayDouble(*v, &bufSize, &bufCount, &emptyBuf[0])
 					if ret == win.PDH_MORE_DATA {
+						counterCount++
 						filledBuf := make([]win.PDH_FMT_COUNTERVALUE_ITEM_DOUBLE, bufCount*size)
 						ret = win.PdhGetFormattedCounterArrayDouble(*v, &bufSize, &bufCount, &filledBuf[0])
 						if ret == win.ERROR_SUCCESS {
 							for i := 0; i < int(bufCount); i++ {
-								//c := filledBuf[i]
-								//s := win.UTF16PtrToString(c.SzName)
-								//
-								//fmt.Printf("%s[%s] = %f\n", k, s, c.FmtValue.DoubleValue)
-								valCount++
+								instanceCount++
 							}
 						}
 					}
 				}
 			} else {
-				fmt.Printf("failed to obtain instances\n")
+				status <- ret
 			}
 
-			fmt.Printf("Collected %d values\n", valCount)
-			time.Sleep(1 * time.Second)
+			fmt.Printf("Counters with values: %d (%d instances)\n", counterCount, instanceCount)
 		}
 	}
 
